50827e80f0a940661b63092ed8efb5f6fe0026e4
weiso131 <blackfire106.1231@gmail.com>
2024-08-27 22:19:45 +0800

    Implement register, edit password

--- Arcaea_score/user_app/urls.py
+++ Arcaea_score/user_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import login, logout
+from .views import login, logout, register, edit_password
 urlpatterns = [
     path("login/", login, name="login"),
     path("logout/", logout, name="logout"),
+    path("register/", register, name="register"),
+    path("edit_password/", edit_password, name="edit_password"),
 ]
--- Arcaea_score/user_app/views.py
+++ Arcaea_score/user_app/views.py
@@ -1,7 +1,19 @@
+import logging
+
 from django.shortcuts import render, redirect, HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.models import User
 from django.contrib import auth
+from django.contrib.auth.hashers import check_password
 
+logger = logging.getLogger(__name__)
+
+def check_user_exist(username):
+    try:
+        User.objects.get(username=username)
+        return True
+    except:
+        return False
 
 # Create your views here.
 
@@ -27,3 +39,43 @@ def login(request):
 def logout(request):
     auth.logout(request)
     return redirect("/ptt_calculator")
+
+
+def register(request):
+    context = {}
+    
+    if request.method == "GET":
+        return render(request, 'user_app/register.html', context)
+    
+    elif request.method == "POST":
+        user_info = request.POST
+        username = user_info['username']
+        password = user_info['password']
+
+        user_exist = check_user_exist(username)
+
+    
+        if user_exist:
+            return render(request, 'user_app/register.html', context)
+        else:
+            user = User.objects.create_user(username=username, password=password)
+            auth.login(request, user)
+            return redirect("/ptt_calculator")
+        
+def edit_password(request):
+    if not request.user.is_authenticated:
+        return redirect("/ptt_calculator")
+    if request.method == "GET":
+        return render(request, 'user_app/edit_password.html', context={"wrong_password" : False})
+    else:
+        user = request.user
+        password = request.POST.get('password', '')
+        new_password = request.POST.get('new_password', '')
+
+        if not check_password(password, user.password):
+            return render(request, 'user_app/edit_password.html', context={"wrong_password" : True})
+        else:
+            user.set_password(new_password)
+            user.save()
+            auth.login(request, user)
+            return redirect("/ptt_calculator")
